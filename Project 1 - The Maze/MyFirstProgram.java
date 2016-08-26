c5606a047f4b5768af1e5ee69308898ab9af741d
lmargain <787105@seq.org>
2016-08-26 08:50:30 -0700

    Add files via upload

---
+++ Project 1 - The Maze/MyFirstProgram.java
@@ -0,0 +1,14 @@
+
+/**
+Luis Oliver Juarez
+MazeWalker
+ */
+public class MyFirstProgram
+{
+    public static void run()
+    {
+        Maze maze = new Maze();
+        MazeWalker mazeWalker = new MazeWalker();
+        mazeWalker.walkMaze(maze.getMazeBot());
+    }
+}
